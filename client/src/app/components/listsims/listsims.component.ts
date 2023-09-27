911c1f8c91de1aebe4bf53e16d7a0ae9f6f8c606
hafiznaeem28 <saqib2448@gmail.com>
2023-09-27 10:27:44 +0400

    new files added

--- client/src/app/components/listsims/listsims.component.ts
+++ client/src/app/components/listsims/listsims.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./listsims.component.css']
 })
 export class ListsimsComponent {
-
+  AddSim():void
+  {
+    console.log("hello world");
+  }
 }
